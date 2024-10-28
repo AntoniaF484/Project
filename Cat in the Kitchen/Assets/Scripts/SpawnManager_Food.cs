200bbcd3ec679ea0b4e0308e648a341d4486e7b4
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-10-28 20:34:27 +0000

    Project- Food Spawns on Path 1 correctly

--- Cat in the Kitchen/Assets/Scripts/SpawnManager_Food.cs
+++ Cat in the Kitchen/Assets/Scripts/SpawnManager_Food.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class SpawnManager_Food : MonoBehaviour
 {
-    private float startDelay = 0.2f;
-    private float spawnIntervalRange = 0.4f;
+    private float startDelay = 0f;
+    //private float spawnIntervalRange = 0.4f;
+private float spawnInterval = 0.6f;
 
-    private float distY = 3.0f;
+   // private float distY = 0.7f;
     public GameObject[] FoodPrefabs; 
-    
+    public Transform spawnLocation; 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomFood", startDelay, (Random.Range(-spawnIntervalRange, spawnIntervalRange)));
+       // InvokeRepeating("SpawnRandomFood", startDelay, spawnInterval);
+SpawnRandomFood();
     }
 
     // Update is called once per frame
@@ -24,8 +26,7 @@ public class SpawnManager_Food : MonoBehaviour
     void SpawnRandomFood()
     {
         int FoodPrefabsIndex = Random.Range(0, FoodPrefabs.Length);
-        Instantiate(FoodPrefabs[FoodPrefabsIndex],
-            new Vector3(3, distY, 0),
-            FoodPrefabs[FoodPrefabsIndex].transform.rotation);
+
+        Instantiate(FoodPrefabs[FoodPrefabsIndex], spawnLocation.position, spawnLocation.rotation);
     }
 }
