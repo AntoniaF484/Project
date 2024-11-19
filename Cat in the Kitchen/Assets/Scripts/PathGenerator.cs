f5188bd47c88e5f0e68c83b045fdaabc4838c8c3
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-11-19 12:50:55 +0000

    Updated Path Generator - Picks random Path Prefab

--- Cat in the Kitchen/Assets/Scripts/PathGenerator.cs
+++ Cat in the Kitchen/Assets/Scripts/PathGenerator.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PathGenerator : MonoBehaviour
 {
     public GameObject platform1;
+    public GameObject[] Path1Prefabs;
+    private float[] platformWidths;
     public Transform generationPoint;
+    private int platformSelector;
     private float distanceBetween;
     public float distanceBetweenMin;
     public float distanceBetweenMax;
@@ -13,17 +16,20 @@ public class PathGenerator : MonoBehaviour
     private float platformWidth;
 
     public ObjectPooler theObjectPool;
-   
-
-   // public GameObject[] Path1Prefabs;
+    
 
     //public GameObject[] Path2Prefabs;
 
     // Start is called before the first frame update
         void Start()
         {
-            platformWidth = platform1.GetComponent<Renderer>().bounds.size.x;
-      
+           // platformWidth = platform1.GetComponent<Renderer>().bounds.size.x;
+           platformWidths = new float [Path1Prefabs.Length];
+           for (int i = 0; i < Path1Prefabs.Length; i++)
+           {
+               platformWidths[i] = Path1Prefabs[i].GetComponent<Renderer>().bounds.size.x;
+           }
+
         }
 
         // Update is called once per frame
@@ -34,14 +40,17 @@ public class PathGenerator : MonoBehaviour
             if (transform.position.x < generationPoint.position.x)
             {
                 distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
-                transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween,
+                platformSelector = Random.Range(0, Path1Prefabs.Length);
+                transform.position = new Vector3(transform.position.x + platformWidths[platformSelector] + distanceBetween,
                     transform.position.y, transform.position.z);
 
-              //  Instantiate(platform1,transform.position, transform.rotation);
-              GameObject newPlatform = theObjectPool.GetPooledObject();
-              newPlatform.transform.position = transform.position;
-              newPlatform.transform.rotation = transform.rotation;
-              newPlatform.SetActive(true);
+                //platformSelector = Random.Range(0, Path1Prefabs.Length);
+
+              Instantiate(Path1Prefabs[platformSelector],transform.position, transform.rotation);
+             // GameObject newPlatform = theObjectPool.GetPooledObject();
+              //newPlatform.transform.position = transform.position;
+              //newPlatform.transform.rotation = transform.rotation;
+              //newPlatform.SetActive(true);
 
             }
         }
