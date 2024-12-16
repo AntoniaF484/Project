a5b8927854dbbd06ee618593a5727a1df2eed116
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-12-16 20:29:56 +0000

    Food Created in Bonus level

---
+++ Cat in the Kitchen/Assets/Scripts/BonusLevelScripts/BonusLevelSpawnManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusLevelSpawnManager : MonoBehaviour
+
+
+{
+
+    public float spawnRange;
+    public GameObject[] foodPrefabs;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("SpawnRandomFood", 1, 0.8f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void SpawnRandomFood()
+    {int foodIndex = Random.Range(0, foodPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRange,spawnRange),1.5f,Random.Range(-spawnRange,spawnRange));
+        Instantiate(foodPrefabs[foodIndex],spawnPos,foodPrefabs[foodIndex].transform.rotation);}
+
+   /* private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+
+    }*/
+
+}
+
