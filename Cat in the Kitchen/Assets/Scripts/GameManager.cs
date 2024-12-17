2393c3786efb4704b0a31cead6422a47eb28d1a4
AntoniaF484 <antoniaforsythe16@gmail.com>
2024-12-17 21:50:53 +0000

    Scene updates

--- Cat in the Kitchen/Assets/Scripts/GameManager.cs
+++ Cat in the Kitchen/Assets/Scripts/GameManager.cs
@@ -144,11 +144,11 @@ public class GameManager : MonoBehaviour
    
        while (isGameActive) 
        {
-           yield return new WaitForSeconds(0.3f);
+           //yield return new WaitForSeconds(0.6f);
            pathGenerator.GeneratePath1();
            pathGenerator.GeneratePath2();
 
-           //yield return new WaitForSeconds(0.5f);
+           yield return new WaitForSeconds(0.5f);
        }
    }
 
