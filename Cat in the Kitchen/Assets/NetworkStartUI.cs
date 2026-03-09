53b5a0894766ea3fcdf94060f58caaafdbfd53f8
AntoniaF484 <antoniaforsythe16@gmail.com>
2026-03-09 21:06:32 +0000

    Player Count Added

--- Cat in the Kitchen/Assets/NetworkStartUI.cs
+++ Cat in the Kitchen/Assets/NetworkStartUI.cs
@@ -1,11 +1,16 @@
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
-public class NetworkStartUI : MonoBehaviour
+public class NetworkStartUI : NetworkBehaviour
 {
     float w = 200f, h = 40f;
 
     float x = 10f, y = 10f;
+    [SerializeField] private TextMeshProUGUI playerCountText;
+    
+    private NetworkVariable<int> playerCount = new NetworkVariable<int>(0,NetworkVariableReadPermission.Everyone);
+
     void OnGUI()
     {
 
@@ -13,12 +18,18 @@ public class NetworkStartUI : MonoBehaviour
             return;
 
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-    {
-        if (GUI.Button(new Rect(x, y, w, h), "Host")) NetworkManager.Singleton.StartHost();
-        if (GUI.Button(new Rect(x, y + h + 10, w, h), "Client")) NetworkManager.Singleton.StartClient();
-        if (GUI.Button(new Rect(x, y + 2 * (h + 10), w, h), "Server")) NetworkManager.Singleton.StartServer();
+        {
+            if (GUI.Button(new Rect(x, y, w, h), "Host")) NetworkManager.Singleton.StartHost();
+            if (GUI.Button(new Rect(x, y + h + 10, w, h), "Client")) NetworkManager.Singleton.StartClient();
+            if (GUI.Button(new Rect(x, y + 2 * (h + 10), w, h), "Server")) NetworkManager.Singleton.StartServer();
 
+        }
     }
-}
+    void Update()
+    {
+        playerCountText.text = playerCount.Value.ToString();
+        if (!IsServer) return;
+        playerCount.Value=NetworkManager.Singleton.ConnectedClients.Count;
+    }
 
 }
