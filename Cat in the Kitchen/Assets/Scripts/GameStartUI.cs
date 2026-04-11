4878a41b7cb1d4145796db4a79193fd00afe6c73
AntoniaF484 <antoniaforsythe16@gmail.com>
2026-04-11 17:37:55 +0100

    Update GameStartUI.cs
    Comments added to explain Game Start UI script

--- Cat in the Kitchen/Assets/Scripts/GameStartUI.cs
+++ Cat in the Kitchen/Assets/Scripts/GameStartUI.cs
@@ -1,45 +1,34 @@
-using System.Collections;
-using UnityEngine;
+
 using TMPro;
-using UnityEngine.UI;
 using Unity.Netcode;
 public class GameStartUI : NetworkBehaviour
 {
     public TMP_InputField nameText;
-    
-    private IndividualPlayerStats thisPlayersStats;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-  
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void OnReadyClicked()
+    public void OnReadyClicked() // when player presses ready button
     {
-        var playerObj = NetworkManager.Singleton.LocalClient.PlayerObject;
+        var playerObj = NetworkManager.Singleton.LocalClient.PlayerObject; // get the players player object from the network manager
 
-        if (playerObj == null)
+        if (playerObj == null) // if the player object hasnt spawned yet, stop. 
         {
             
             return;
         }
 
-        var stats = playerObj.GetComponent<IndividualPlayerStats>();
+        var stats = playerObj.GetComponent<IndividualPlayerStats>(); // get this players inputted stats (name and ready status in this case)
 
         if (stats == null)
         {
             return;
         }
 
-        stats.SetPlayerNameServerRpc(nameText.text);
-        stats.SetReadyServerRpc(true);
+        stats.SetPlayerNameServerRpc(nameText.text); // sets the players inputted name. Update network varialbe so everyone can see it
+        stats.SetReadyServerRpc(true); // sets the player as ready
         
         
-        FindObjectOfType<GameManager>().PlayerReadyServerRpc();
+        FindObjectOfType<GameManager>().PlayerReadyServerRpc(); //notify game manager there is +1 ready player
 
-        gameObject.SetActive(false);
+        gameObject.SetActive(false); // hide ui for player once theyre ready
 
     }
 }
