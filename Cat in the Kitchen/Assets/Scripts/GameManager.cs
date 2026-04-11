d02a0251497d4b030ccaa92d166a4e373cf63e58
AntoniaF484 <antoniaforsythe16@gmail.com>
2026-04-11 17:27:09 +0100

    Update GameManager.cs
    added explanatory comments to gamemanger

--- Cat in the Kitchen/Assets/Scripts/GameManager.cs
+++ Cat in the Kitchen/Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
-using System.Collections;
+
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
@@ -11,137 +10,121 @@ using Unity.Netcode;
 public class GameManager : NetworkBehaviour
 
 {
+    
+    //UI
+        //UI For players own view
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI livesText;
+    
+        //UI For world view
     public TextMeshProUGUI gameOverText;
     public TextMeshProUGUI hiScoreText;
     public Button replayButton;
     public GameObject titleScreen;
-
+        //Start game UI
+        public TMP_Dropdown expectedPlayersDropdown;
+        // Scoreboard
+        public TextMeshProUGUI firstPlaceScore;
+        private int firstScore;
+        public TextMeshProUGUI secondPlaceScore;
+        private int secondScore;
+        public TextMeshProUGUI thirdPlaceScore;
+        private int thirdScore;
+        
+//Other scripts
     private PlayerController playerController;
     private PathGenerator pathGenerator;
+    private PowerUpManager powerUpManager;
 
    
 
-
+// Player movement
     private int score;
     public static int totalScore;
     public int addedScore;
+//player stats
     public int lives;
     public static int totalLives;
     public int hiScore;
-
-    public bool isGameActive;
+    
+    //Path generation/game setup
+   public bool isGameActive;
     public Transform PathGenerator;
+    public bool returnFromBonusLevel;
+    public int autoDifficulty = 3;
 
-    //Distance Between Path Variables
+         //Distance Between Path Variables
     private float distanceBetweenMinPath1;
     private float distanceBetweenMinPath2;
 
     private float distanceBetweenMaxPath1;
     private float distanceBetweenMaxPath2;
 
-
-    // PowerUps
-    private PowerUpManager powerUpManager;
-    
-    public TextMeshProUGUI firstPlaceScore;
-    private int firstScore;
-    public TextMeshProUGUI secondPlaceScore;
-    private int secondScore;
-    public TextMeshProUGUI thirdPlaceScore;
-    private int thirdScore;
-
-
-    public bool returnFromBonusLevel;
-   
-    public int autoDifficulty = 3;
-    
-    
-  
     private float originalDistanceMax; 
     private float originalDistanceMin;
-    
-    
-    [SerializeField] private GameObject [] playerPrefab;
-    private List<GameObject> availableCats;
-    
-    
+   
+
+
+   // Player joining
+    [SerializeField] private GameObject [] playerPrefab; //possible cat prefabs to spawn when the player joins
+    private List<GameObject> availableCats; // list of available prefabs (not yet spawned)
     
     public NetworkVariable<bool> allReady = new(readPerm: NetworkVariableReadPermission.Everyone,
-        writePerm: NetworkVariableWritePermission.Server);
+        writePerm: NetworkVariableWritePermission.Server); //bool to mark if all the players in the game are ready for it to start
 
-    public int defaultExpectedPlayers=4;
-    private int readyPlayers;
-    public TMP_Dropdown expectedPlayersDropdown;
+    public int defaultExpectedPlayers=4;// pre-setting default players to 4 (max possible) allows other players to join before the host has set expected players. Not concerned with too many joining as expected players is to make sure the game doesnt start before all players have joined and are ready
+    private int readyPlayers; // total plays who have pressed ready button
+    public GameObject joinScreen; //join screen which will be deactivated once the game starts
 
-    public GameObject joinScreen;
-
-    private int spawnedCats = 0;
-    public NetworkVariable<int> expectedPlayers = new(
-        4,
-        NetworkVariableReadPermission.Everyone,
-        NetworkVariableWritePermission.Server
-    );
+    private int spawnedCats = 0; //number of players who have joined the game
+    public NetworkVariable<int> expectedPlayers = new(4, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);//number of players expected
 
     void Start()
     
     {
         if (NetworkManager.Singleton.IsServer)
         {
-            expectedPlayers.Value = defaultExpectedPlayers;
+            expectedPlayers.Value = defaultExpectedPlayers; // at the start of the game, expected players is set by the server as the default expected players
         }
-        availableCats =new List<GameObject>(playerPrefab);
+        
+        availableCats =new List<GameObject>(playerPrefab); // list of available cat prefabs
         if (NetworkManager.Singleton != null)
         {
-            NetworkManager.Singleton.OnClientConnectedCallback += SpawnCatForClient;
+            NetworkManager.Singleton.OnClientConnectedCallback += SpawnCatForClient; // cat is spawned when a player connects
         }
+        
         pathGenerator = FindObjectOfType<PathGenerator>();
-      
         powerUpManager = FindObjectOfType<PowerUpManager>();
      
     }
-    public void OnPlayerCountChanged(int index)
+    public void OnPlayerCountChanged(int index) // linked to player count dropdown
     {
-        if (!NetworkManager.Singleton.IsServer) return;
+        if (!NetworkManager.Singleton.IsServer) return; // expected players is only set by the server
 
-        expectedPlayers.Value = index;
-        
-        
+        expectedPlayers.Value = index; // sets expected players. Index corresponds to the position of the number of players in the dropdown list
     }
 
-    public void StartGame (int difficulty) // distance between generated paths increases with difficulty selected
+    public void StartGame (int difficulty) // distance between generated paths increases with difficulty set 
     {
-        
-        
-        
-        
         isGameActive = true;
         pathGenerator=FindObjectOfType<PathGenerator> ();
-        
-       
-        
+  
      pathGenerator.distanceBetweenMinPath1 *= difficulty;
      pathGenerator.distanceBetweenMaxPath1 *= difficulty;
-      
-     
+  
      pathGenerator.distanceBetweenMinPath2 *= difficulty; 
-     pathGenerator.distanceBetweenMaxPath2 *= difficulty;
+     pathGenerator.distanceBetweenMaxPath2 *= difficulty; 
      
-     
-     
-        titleScreen.gameObject.SetActive(false);
+     titleScreen.gameObject.SetActive(false);
      
     }
     
 
-    public void GameOver()
+    public void GameOver() // when the game is over, this brings up the game over message and stops the players movement
     {
         gameOverText.gameObject.SetActive(true);
         replayButton.gameObject.SetActive(true);
         isGameActive = false;
-        
-        
         PlayerController player = FindObjectOfType<PlayerController>();
         player.enableMovement = false;
         Rigidbody playerRb = FindObjectOfType<PlayerController>().GetComponent<Rigidbody>();
@@ -150,36 +133,29 @@ public class GameManager : NetworkBehaviour
 
 
     }
-
-    public void RestartGame()
-    {
-        isGameActive = true;
-        SceneManager.LoadScene("MyGame");
-        totalScore = 0;
-    }
-
+    
     void Update()
     {
-        if (allReady.Value){ joinScreen.gameObject.SetActive(false);}
+        if (allReady.Value){ joinScreen.gameObject.SetActive(false);} // if all players are ready, deactivate the join game screen
     }
     private void SpawnCatForClient(ulong clientId)
     {
         if (!NetworkManager.Singleton.IsServer) return; //only server can spawn cats
-        if (spawnedCats >= expectedPlayers.Value)
+        if (spawnedCats >= expectedPlayers.Value) // do not spawn more cats if expected players is exceeded
         {
             return;
         }
 
-        spawnedCats++;
+        spawnedCats++;// spawns a new cat
         int prefabIndex = Random.Range(0, availableCats.Count); //pick number at random based on number of cat prefabs
         GameObject prefabToSpawn = availableCats [prefabIndex]; //corresponding cat to be spawned
-        availableCats.RemoveAt(prefabIndex);
+        availableCats.RemoveAt(prefabIndex); // remove spawned cat from list
         GameObject playerInstance = Instantiate(prefabToSpawn); //in server/host the prefab is instantiated
         NetworkObject netObj = playerInstance.GetComponent<NetworkObject>(); //get the nework object on the spawned cat (to synch with Client)
         netObj.SpawnAsPlayerObject(clientId, true); //Client owns this playerobject
         
         IndividualPlayerStats playerStats = playerInstance.GetComponent<IndividualPlayerStats>();
-        if (playerStats != null)
+        if (playerStats != null) // sets the players starting lives and score
         {
             playerStats.lives.Value = playerStats.startingLives;
             playerStats.score.Value = 0;
@@ -188,9 +164,9 @@ public class GameManager : NetworkBehaviour
     }
     
     [ServerRpc(RequireOwnership = false)]
-    public void PlayerReadyServerRpc()
+    public void PlayerReadyServerRpc() // when a player clicks that they are ready, add one to ready players. When readyplayers matches expected players, everyone is ready (allready=true)
     {
-        readyPlayers++ ;
+        readyPlayers++ ; 
 
         if (readyPlayers >= expectedPlayers.Value)
         {
@@ -202,7 +178,7 @@ public class GameManager : NetworkBehaviour
     {
         if (NetworkManager.Singleton != null)
         {
-            expectedPlayersDropdown.gameObject.SetActive(NetworkManager.Singleton.IsHost);
+            expectedPlayersDropdown.gameObject.SetActive(NetworkManager.Singleton.IsHost);// only the host can see the expected player number dropdown
         }
         
     }
