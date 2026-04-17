0a9ad0258d6f27c0a40fa051e79631c51f44a66f
AntoniaF484 <antoniaforsythe16@gmail.com>
2026-04-17 10:19:56 +0100

    Online multiplayer working

--- Cat in the Kitchen/Assets/Scripts/RelayManager.cs
+++ Cat in the Kitchen/Assets/Scripts/RelayManager.cs
@@ -18,15 +18,22 @@ public class RelayManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI joinCodeText;
 
     [SerializeField] private TMP_InputField joinCodeInputField;
+    private bool isInitialized;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private async void Start()
     {
-        UnityServices.InitializeAsync();
+        await UnityServices.InitializeAsync();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        isInitialized = true;
     }
 
     public async void StartRelay()
     {
+        if (!isInitialized)
+        {
+            Debug.Log("Services not initialized yet");
+            return;
+        } 
         string joinCode = await StartHostWithRelay();
         joinCodeInputField.text = joinCode;
     }
@@ -40,7 +47,9 @@ public class RelayManager : MonoBehaviour
     {
         Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
         RelayServerData relayServerData = allocation.ToRelayServerData("wss");
+        NetworkManager.Singleton.GetComponent<UnityTransport>().UseWebSockets = true;
       NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+      
         string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
         return NetworkManager.Singleton.StartHost() ? joinCode : null;
            
@@ -50,7 +59,9 @@ public class RelayManager : MonoBehaviour
     {
         JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
         RelayServerData relayServerData = joinAllocation.ToRelayServerData("wss");
+        NetworkManager.Singleton.GetComponent<UnityTransport>().UseWebSockets = true;
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+        
         return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient(); 
     }
 }
